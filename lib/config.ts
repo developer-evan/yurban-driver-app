c62e3e8ca752dbc5519b7348d15550224e88de01
Mogeni <evansmogeni254@gmail.com>
2024-11-23 23:07:02 +0300

    update api url

--- lib/config.ts
+++ lib/config.ts
@@ -1,13 +1,13 @@
 export interface AppConfig {
-    apiUrl: string;
-    // appUrl:string;
-    // appName:string
-  }
+  apiUrl: string;
+  // appUrl:string;
+  // appName:string
+}
 
 const config: AppConfig = {
   apiUrl: "https://yurban-server-2.onrender.com/api",
-//   appUrl: process.env.REACT_APP_APP_URL as string,
-//   appName: process.env.REACT_APP_APP_NAME as string,
+  //   appUrl: process.env.REACT_APP_APP_URL as string,
+  //   appName: process.env.REACT_APP_APP_NAME as string,
 };
 
 export default config;
